57222e93e90508cb819438adc8a118db5064d2da
Tak Ohta <sacredfield.tak@gmail.com>
2020-08-10 04:56:11 +0000

    4章4.4 Finish validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  
+  # メールアドレスの⼤⽂字⼩⽂字を区別せず⼩⽂字として登録（before_saveというコールバックメソッドを使用する）。オブジェクトが保存される時点で処理を実行する。
+  before_save {self.email = email.downcase}
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  # メールアドレスの正規表現（正規表現regex = regular expression）
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: true
 end
