c2cf9eecd70b8cf911ba6688fbbb2f661c291dea
Tak Ohta <sacredfield.tak@gmail.com>
2020-08-15 05:11:43 +0000

    5章5.5.4 Finish user-information-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存に成功した場合の処理
+    else
+      render :new
+    end
+  end
+  
+  private
+    # Usersコントローラー内部でのみ実行される
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
