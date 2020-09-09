96539127e25e9bd40e02611d24769321c761d4ad
Tak Ohta <sacredfield.tak@gmail.com>
2020-09-09 13:07:31 +0000

    9章9.1 Finish add-department-column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
   private
     # Usersコントローラー内部でのみ実行される
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
     
     # paramsハッシュからユーザーを取得する
