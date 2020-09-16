970f7ddabe949acbb631ae6bfdb3c1412d18971d
Tak Ohta <sacredfield.tak@gmail.com>
2020-09-16 10:14:02 +0000

    10章10.7 Finish

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,10 @@
 module AttendancesHelper
+  def attendance_state(attendance)
+    # 受け取ったattendanceオブジェクトが当日と一致するか評価する。
+    if Date.current == attendance.worked_on
+      return "出勤" if attendance.started_at.nil?
+      return "退勤" if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    false
+  end
 end
